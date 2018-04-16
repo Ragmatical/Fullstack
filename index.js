e4a6bb509e20996f34cf8b4143109d0069711a1a
Ragmatical <37642718+Ragmatical@users.noreply.github.com>
2018-04-15 19:38:35 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* The express module is used to look at the address of the request and send it to the correct function */
 var express = require('express');
 var bodyParser = require('body-parser');
+var Io = require('socket.io')
 /* The http module is used to listen for requests from a web browser */
 var http = require('http');
 
@@ -13,22 +14,34 @@ var mongoose = require('mongoose')
 /* The path module is used to transform relative paths to absolute paths */
 var path = require('path');
 
-
-
 /* Creates an express application */
 var app = express();
 
 /* Creates the web server */
 var server = http.createServer(app);
 
+var io = Io(server)
+
 /* Defines what port to use to listen to web requests */
 var port =  process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
 var dbAddress = process.env.MONGODB_URI || 'mongodb://127.0.0.1/game'
 
+function addSockets() {
+	io.on('connection', (socket) => {
+	console.log('user connected')
+	socket.on('disconnect', () => {
+	console.log('user.disconnected');
+	})
+})
+}
 
 function startServer() {
+
+	addSockets();
+
 	app.use(bodyParser.json({ limit: '16mb' }));
+	app.use(express.static(path.join(__dirname, 'public')));
 	/* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
 	app.get('/surveymonkey', (req, res, next) => {
 
@@ -36,6 +49,12 @@ function startServer() {
 		var filePath = path.join(__dirname, './index.html')
 
 		/* Sends the html file back to the browser */
+		res.sendFile(filePath);
+	});
+
+	app.get('/game', (req, res, next) => {
+
+		var filePath = path.join(__dirname, '/game.html')
 		res.sendFile(filePath);
 	});
 
