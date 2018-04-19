7e8fb8931b9346348866c7c9710b5fe090d4614f
ragmatical <junokimzone@gmail.com>
2018-04-18 19:59:55 -0700

    added login

--- index.js
+++ index.js
@@ -1,50 +1,137 @@
 /* The express module is used to look at the address of the request and send it to the correct function */
 var express = require('express');
-
-/* The http module is used to listen for requests from a web browser */
+var bodyParser = require('body-parser');
+var Io = require('socket.io')
 var http = require('http');
-
-/* The path module is used to transform relative paths to absolute paths */
+var usermodel = require('./user.js').getModel();
+var crypto = require('crypto');
+var mongoose = require('mongoose')
 var path = require('path');
-
-/* Creates an express application */
 var app = express();
-
-/* Creates the web server */
 var server = http.createServer(app);
-
+var io = Io(server)
+var iterations = 10000;
 /* Defines what port to use to listen to web requests */
 var port =  process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
+var dbAddress = process.env.MONGODB_URI || 'mongodb://127.0.0.1/game'
 
-/* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
-app.get('/surveymonkey', (req, res, next) => {
+function addSockets() {
+	io.on('connection', (socket) => {
+	console.log('user connected')
+	socket.on('disconnect', () => {
+	console.log('user.disconnected');
+	})
+})
+}
 
-	/* Get the absolute path of the html file */
-	var filePath = path.join(__dirname, './index.html')
+function startServer() {
 
-	/* Sends the html file back to the browser */
-	res.sendFile(filePath);
-});
+	function authenticateUser(username, password, callback) {
 
-app.get('/', (req, res, next) => {
-  var filePath = path.join(__dirname, './home.html')
-	res.sendFile(filePath);
-});
+		if(!username) return callback('No username given');
+		if(!password) return callback('No password given');
+		usermodel.findOne({username: username}, (err, user) => {
+			if(err) return callback('Error connecting to database');
+			if(!user) return callback('Incorrect Username');
+			crypto.pbkdf2(password, user.salt, iterations, 256, 'sha256', (err, resp) => {
+				if(err) return callback('Error handling password');
+				if(resp.toString('base64')=== user.password) return callback(null);
+				callback('Incorrect password');
+			});
+		});
+	}
 
-/* Defines what function to all when the server recieves any request from http://localhost:8080 */
-server.on('listening', () => {
+	addSockets();
 
-	/* Determining what the server is listening for */
-	var addr = server.address()
-		, bind = typeof addr === 'string'
-			? 'pipe ' + addr
-			: 'port ' + addr.port
-	;
+	app.use(bodyParser.json({ limit: '16mb' }));
+	app.use(express.static(path.join(__dirname, 'public')));
+	/* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
+	app.get('/surveymonkey', (req, res, next) => {
 
-	/* Outputs to the console that the webserver is ready to start listenting to requests */
-	console.log('Listening on ' + bind);
-});
+		/* Get the absolute path of the html file */
+		var filePath = path.join(__dirname, './index.html')
 
-/* Tells the server to start listening to requests from defined port */
-server.listen(port);
+		/* Sends the html file back to the browser */
+		res.sendFile(filePath);
+	});
+
+	app.get('/game', (req, res, next) => {
+
+		var filePath = path.join(__dirname, '/game.html')
+		res.sendFile(filePath);
+	});
+
+	app.get('/login', (req, res, next) => {
+		var filePath = path.join(__dirname, './login.html')
+		res.sendFile(filePath);
+	});
+
+	app.get('/', (req,res,next) => {
+		var filePath = path.join(__dirname, './home.html')
+		res.sendFile(filePath)
+	});
+
+	app.get('/hw1', (req,res,next) => {
+		var filePath = path.join(__dirname, './Hw1.html')
+		res.sendFile(filePath)
+	});
+
+	app.post('/', (req, res, next) => {
+	  var filePath = path.join(__dirname, './home.html')
+		res.sendFile(filePath);
+	});
+
+	app.post('/login', (req, res, next) => {
+		var username = req.body.username;
+		var password = req.body.password;
+		authenticateUser(username, password, (err) => {
+			res.send({error:err});
+		});
+	});
+
+	app.post('/surveymonkey', (req, res, next) => {
+		var newuser = new usermodel(req.body);
+		var password = req.body.password;
+		var salt = crypto.randomBytes(128).toString('base64');
+		newuser.salt = salt;
+		// Winding up the crypto hashing lock 10000 times
+		crypto.pbkdf2(password, salt, iterations, 256, 'sha256', function(err, hash) {
+			if(err) {
+				return res.send({error: err});
+			}
+			newuser.password = hash.toString('base64');
+			// Saving the user object to the database
+			newuser.save(function(err) {
+
+			// Handling the duplicate key errors from database
+			if(err && err.message.includes('duplicate key error') && err.message.includes('userName')) {
+				return res.send({error: 'Username, ' + req.body.userName + 'already taken'});
+			}
+			if(err) {
+				return res.send({error: err.message});
+			}
+				res.send({error: null});
+			});
+		});
+	});
+
+	/* Defines what function to all when the server recieves any request from http://localhost:8080 */
+	server.on('listening', () => {
+
+
+		/* Determining what the server is listening for */
+		var addr = server.address()
+			, bind = typeof addr === 'string'
+				? 'pipe ' + addr
+				: 'port ' + addr.port
+		;
+
+		/* Outputs to the console that the webserver is ready to start listenting to requests */
+		console.log('Listening on ' + bind);
+	});
+
+	/* Tells the server to start listening to requests from defined port */
+	server.listen(port);
+}
+mongoose.connect(dbAddress, startServer)
